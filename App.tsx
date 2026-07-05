847f13192a644a03cc7dac583b1022788eb0b016
honoxia <serefdincer@gmail.com>
2026-07-06 00:09:28 +0300

    Force dark theme across devices

--- App.tsx
+++ App.tsx
@@ -96,7 +96,7 @@ export default function App() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider>
+      <ThemeProvider forcedTheme="dark">
         <AppContent />
         <Toast />
       </ThemeProvider>
